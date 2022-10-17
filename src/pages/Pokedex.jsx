75db52a6e4730492896093d8722fabac21a41785
David Portilla <david77fpm@gmail.com>
2022-10-17 18:20:57 -0500

    poke api

---
+++ src/pages/Pokedex.jsx
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import CardPoke from '../components/pokedex/CardPoke'
+import InputSearch from '../components/pokedex/InputSearch'
+import SelecByType from '../components/pokedex/SelecByType'
+
+const Pokedex = () => {
+
+  const [pokemons, setPokemons] = useState()
+  const [typeSelected, setTypeSelected] = useState('All Pokemons')
+
+  useEffect(() => {
+    if (typeSelected !== 'All Pokemons'){
+      // si se seleccionó un tipo
+      axios.get(typeSelected)
+      
+      //.then(res => {
+        //const result = res.data.pokemon.map((e) => e.pokemon)
+        //setPokemons(result)
+        //catch(err => console.log(err))      
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+   
+  } else {
+    // si quiero todos los pokemons
+    const URL = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+    axios.get(URL)
+    .then(res => setPokemons(res.data.results))
+    .catch (err => console.log(err))
+  }
+    
+  },[typeSelected])
+
+  const userName = useSelector(state => state.userName)
+
+  return (
+    <div>
+      <header>
+        <h1>Pokedex</h1>
+        <p>Welcome <span>{userName}</span>, here you can find your favorite pokemon.</p>
+      </header>
+      <aside>
+        <InputSearch />
+        <SelecByType setTypeSelected={setTypeSelected}/>
+      </aside>
+      <main>
+        <div className='card--container'>
+            {
+              pokemons?.map(pokemon => (
+                <CardPoke
+                    key={pokemon.url}
+                    url={pokemon.url}
+                />
+              ))
+            }    
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Pokedex
